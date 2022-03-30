7a9d3c3fc93cb0b251e6f2c4cb69cef03fae68ef
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-03-30 12:58:45 +0100

    ooft, forgot to commit each test

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,41 @@
+def return_10():
+    return 10
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+
+def length_of_string(a):
+    return len(a)
+
+
+def join_string(a, b):
+    return a + b
+
+
+def add_string_as_number(a, b):
+    c = int(a)+int(b)
+    return c
+
+
+def number_to_full_month_name(months):
+    dict = {1: "January", 3: "March", 9: "September"}
+    return dict[months]
+
+
+def number_to_short_month_name(short_month):
+    dict = {1: "Jan", 4: "Apr", 10: "Oct"}
+    return dict[short_month]
---
+++ start_point/src/test.py
@@ -0,0 +1,5 @@
+dict = {1: "January", 3: "March", 9: "September"}
+print(dict[1])
+
+ 
+len_side ** 3
